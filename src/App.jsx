d5bcdd8d13d0d2ccf7eacdb2883c6800384eba69
learning2ProgramNF <nick.finn6@gmail.com>
2025-12-04 16:33:21 -0700

    feat: complete Meet app with PWA, charts, and offline functionality

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import { extractLocations, getEvents } from './api';
 import { InfoAlert, ErrorAlert, WarningAlert } from './components/Alert';
 import './App.css';
 import CityEventsChart from './components/CityEventsChart';
+import EventGenresChart from './components/EventGenresChart';
 
 const App = () => {
   const [allLocations, setAllLocations] = useState([]);
@@ -56,8 +57,11 @@ const App = () => {
         setCurrentNOE={setCurrentNOE}
         setErrorAlert={setErrorAlert}
       />
-
-      <CityEventsChart allLocations={allLocations} events={events} />
+      
+      <div className="charts-grid">
+        <EventGenresChart events={events} />
+        <CityEventsChart allLocations={allLocations} events={events} />
+      </div>
 
       <EventList events={events} />
     </div>
