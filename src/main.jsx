142c921293ae3b93eb849cdb4da56e07c556ef9e
learning2ProgramNF <nick.finn6@gmail.com>
2025-12-03 17:06:37 -0700

    feat: added PWA functionality

--- src/main.jsx
+++ src/main.jsx
@@ -4,14 +4,18 @@ import * as atatus from 'atatus-spa';
 atatus.config('3843fb33ac34415b88d21c8bce90a7d3').install(); 
 
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+ <React.StrictMode>
+   <App />
+ </React.StrictMode>,
+);
+serviceWorkerRegistration.register();
 
 
